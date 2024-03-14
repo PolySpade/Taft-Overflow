4738eec4059383f743a4fa83632c0c243fd92f5b
TheRealSunder <darylle_calugtong@dlsu.edu.ph>
2024-03-14 23:22:49 +0800

    BEM and images in proper folder

--- client/src/containers/body/Body.jsx
+++ client/src/containers/body/Body.jsx
@@ -1,24 +1,26 @@
-import React from 'react'
+import React from 'react';
 import './body.css';
-import {Post} from '../../components/index';
-
+import { Post } from '../../components/index';
+import course_cover from '../../assets/images/csarch1cover.jpg';
+import course_picture from '../../assets/images/csarch1logo.png';
 
 const Body = () => {
   return (
     <div className='body__container section__padding'>
       <div className='header'>
-        <div class="cover-photoappdev"></div>
-        <div class="profile-container">
-            <div class="profile-pictureappdev"></div>
-            <div class="profilecontainer">
-            <div class="course-name">c/CSARCH1</div>
-            <button class="right-button">Join</button>
-            </div>
+        <div className="header__cover-photo" style={{ backgroundImage: `url(${course_cover})` }}></div>
+        <div className="header__profile-container">
+        <div className="header__course-picture" style={{ backgroundImage: `url(${course_picture})` }}></div>
+          <div className="header__profilecontainer">
+            <div className="header__course-name">c/CSARCH1</div>
+            <button className="header__right-button">Join</button>
+          </div>
         </div>
+      </div>
+      <Post></Post>
     </div>
-    <Post></Post>
-  </div>
-  )
+  );
 }
 
-export default Body
+
+export default Body;
